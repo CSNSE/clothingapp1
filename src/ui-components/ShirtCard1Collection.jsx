77b8acfe643abab79621f92a34b565b1600a9a1e
tyler <mastrangelo.tyler@gmail.com>
2024-02-04 12:49:50 -0500

    https://github.com/CSNSE/clothingapp1/issues/3

--- src/ui-components/ShirtCard1Collection.jsx
+++ src/ui-components/ShirtCard1Collection.jsx
@@ -1,9 +1,4 @@
-/***************************************************************************
- * The contents of this file were generated with Amplify Studio.           *
- * Please refrain from making any modifications to this file.              *
- * Any changes to this file will be overwritten when running amplify pull. *
- **************************************************************************/
-
+// ShirtCard1Collection.js
 /* eslint-disable */
 import * as React from "react";
 import { listShirts } from "../graphql/queries";
@@ -11,9 +6,12 @@ import ShirtCard1 from "./ShirtCard1";
 import { getOverrideProps } from "./utils";
 import { Collection, Pagination, Placeholder } from "@aws-amplify/ui-react";
 import { generateClient } from "aws-amplify/api";
+import { getUrl } from "@aws-amplify/storage"; 
+
 const nextToken = {};
 const apiCache = {};
 const client = generateClient();
+
 export default function ShirtCard1Collection(props) {
   const { items: itemsProp, overrideItems, overrides, ...rest } = props;
   const [pageIndex, setPageIndex] = React.useState(1);
@@ -23,65 +21,77 @@ export default function ShirtCard1Collection(props) {
   const [instanceKey, setInstanceKey] = React.useState("newGuid");
   const [loading, setLoading] = React.useState(true);
   const [maxViewed, setMaxViewed] = React.useState(1);
-  const pageSize = 1;
+  const pageSize = 1; // Adjust the page size as needed
   const isPaginated = true;
+
   React.useEffect(() => {
     nextToken[instanceKey] = "";
     apiCache[instanceKey] = [];
   }, [instanceKey]);
+
   React.useEffect(() => {
     setIsApiPagination(!!!itemsProp);
   }, [itemsProp]);
-  const handlePreviousPage = () => {
-    setPageIndex(pageIndex - 1);
-  };
-  const handleNextPage = () => {
-    setPageIndex(pageIndex + 1);
-  };
-  const jumpToPage = (pageNum) => {
-    setPageIndex(pageNum);
-  };
+
+  const handlePreviousPage = () => setPageIndex(pageIndex - 1);
+  const handleNextPage = () => setPageIndex(pageIndex + 1);
+  const jumpToPage = (pageNum) => setPageIndex(pageNum);
+
   const loadPage = async (page) => {
     const cacheUntil = page * pageSize + 1;
     const newCache = apiCache[instanceKey].slice();
     let newNext = nextToken[instanceKey];
+
     while ((newCache.length < cacheUntil || !isPaginated) && newNext != null) {
       setLoading(true);
-      const variables = {
-        limit: pageSize,
-      };
-      if (newNext) {
-        variables["nextToken"] = newNext;
-      }
+      const variables = { limit: pageSize };
+      if (newNext) variables["nextToken"] = newNext;
+
       const result = (
         await client.graphql({
           query: listShirts.replaceAll("__typename", ""),
           variables,
         })
       ).data.listShirts;
+
       newCache.push(...result.items);
       newNext = result.nextToken;
+
+      const shirtsFromAPI = result.items;
+      await Promise.all(
+        shirtsFromAPI.map(async (shirt) => {
+          if (shirt.Image) {
+            const urlResult = await getUrl({ key: shirt.Image });
+            shirt.Image = urlResult.url;
+          }
+          return shirt;
+        })
+      );
     }
+
     const cacheSlice = isPaginated
       ? newCache.slice((page - 1) * pageSize, page * pageSize)
       : newCache;
+
     setItems(cacheSlice);
     setHasMorePages(!!newNext);
     setLoading(false);
     apiCache[instanceKey] = newCache;
     nextToken[instanceKey] = newNext;
   };
+
   React.useEffect(() => {
     loadPage(pageIndex);
   }, [pageIndex]);
+
   React.useEffect(() => {
     setMaxViewed(Math.max(maxViewed, pageIndex));
   }, [pageIndex, maxViewed, setMaxViewed]);
+
   return (
     <div>
       <Collection
         type="list"
-        searchPlaceholder="Search..."
         direction="column"
         justifyContent="left"
         itemsPerPage={pageSize}
