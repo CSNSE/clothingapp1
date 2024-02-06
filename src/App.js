3f4a6f6da4d15f0e6513659c21007884393e4caa
tyler <mastrangelo.tyler@gmail.com>
2024-02-06 13:09:44 -0500

    phone css update 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { NavBar1, NoteCreateForm, ShirtCard1Collection, ShirtCard2Collection, ShirtCreateForm } from './ui-components';
+import { NavBar1, ShirtCard1Collection, ShirtCard2Collection, ShirtCreateForm } from './ui-components';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import "./App.css";
 
 const App = () => {
   return (
     <Router>
-      <div className="ui-component-container">
-        <Routes>
-          <Route path="/" element={<div><NavBar1/><ShirtCard2Collection /></div>} />
-          <Route path="/other" element={<NoteCreateForm />} />
-          <Route path="/shirt" element={<div><NavBar1/><ShirtCreateForm /></div>} />
-          <Route path="/shirtcard1collection" element={<ShirtCard1Collection />} />
-        </Routes>
+      <div className="App"> {/* Apply the "App" class to the main container */}
+        <div className="ui-component-container">
+          <Routes>
+            <Route path="/" element={<div><NavBar1/><ShirtCard2Collection /></div>} />
+            <Route path="/shirt" element={<div><NavBar1/><ShirtCreateForm /></div>} />
+            <Route path="/shirtcard1collection" element={<ShirtCard1Collection />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
